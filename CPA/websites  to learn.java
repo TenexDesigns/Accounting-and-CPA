a49766d7880ba6c6c73cc6ab461da7be5b3b5c49
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-07-27 21:03:08 +0300

    Create websites  to learn.java

---
+++ CPA/websites  to learn.java
@@ -0,0 +1,5 @@
+https://www.studocu.com/row/document/maseno-university/financial-management/cpa-reading-list-coa/10809281
+https://kasneb.or.ke/wp-content/uploads/2022/05/CPA-Recommended-Reading-List-1.pdf
+https://kasnebnotes.co.ke/financial-accounting-past-exam-december-2021/
+https://www.kasneb.or.ke/reading-lists/
+https://docs.google.com/spreadsheets/d/e/2PACX-1vSHvV7un2de6bIzivLows44FbfMwJ5Ih7gLfs4f_POEeMSSYYks3dz8lyRVZJwlYNGwIFdmX6BjFmnX/pubhtml?gid=1507857810&single=true
