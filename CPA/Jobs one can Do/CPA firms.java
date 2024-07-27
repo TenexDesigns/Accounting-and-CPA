5f9d07c13c5966cd6bd29027072f927dc1fe92eb
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2024-07-27 14:55:25 +0300

    Create CPA firms.java

---
+++ CPA/Jobs one can Do/CPA firms.java
@@ -0,0 +1,20 @@
+https://en.wikipedia.org/wiki/List_of_accounting_firms_in_Kenya
+
+
+
+This is a list of accounting companies operating in Kenya. They are regulated by the Institute of Certified Public Accountants of Kenya:[1]
+
+PwC Kenya[2]
+Deloitte[3]
+BDO East Africa[4]
+KPMG East Africa[5]
+Ernst & Young[6]
+Baker Tilly International[7]
+PKF Eastern Africa[8]
+HLB International[9]
+RSM E.A.[10]
+BDO East Africa[4]
+Grant Thornton[11]
+Crowe Global[12]
+Mazars[13]
+Patrick and Associates[14]
